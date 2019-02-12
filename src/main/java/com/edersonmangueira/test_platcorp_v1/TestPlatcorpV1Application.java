1733538c5f5bdf0de690ff7ed3f3e19b45247f0a
edersonfn <edersonfm@hotmail.com>
2019-02-12 19:23:37 -0200

    Criando projeto

---
+++ src/main/java/com/edersonmangueira/test_platcorp_v1/TestPlatcorpV1Application.java
@@ -0,0 +1,14 @@
+package com.edersonmangueira.test_platcorp_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPlatcorpV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPlatcorpV1Application.class, args);
+	}
+
+}
+
